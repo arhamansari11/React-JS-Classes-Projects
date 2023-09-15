158e288c4ecd8c97339215c0ed1dc0ebe3a6310b
Arham <arhamansari454647@gmail.com>
2023-09-15 08:45:36 +0500

    Class 01

---
+++ Class 01/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <>
+      <h1 className="h1">Hello How are You Friends?</h1>
+      <h1 className="h1">It's My First react Application</h1>
+      <h1 className="h1">I share my all Projects of React and my all practices of React on GitHub</h1>
+      <h1 className="h1">You see my all Projects on GitHub.</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
